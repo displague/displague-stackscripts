95dcd6869fa27ef180c962726c9235a62a05484b
Marques Johansson <marques@linode.com>
2014-08-22 23:59:42 -0400

    first commit of https://www.linode.com/stackscripts/view/10079

---
+++ 10079/README.md
@@ -0,0 +1,17 @@
+# Rootless and GitHub User has sudo
+
+Sick of passwords?  Have a GitHub account configured with your favorite ssh keys?
+
+You've come to the right place.  This StackScript removes the barrier to entry.  The root password has been disabled, so you can't ssh in as root.  But wait, root is not out of reach!  
+
+With one simple text entry of your GitHub username, all of your GitHub configured ssh keys will be authorized for login with your existing GitHub username.  Root password? Who needs it.  While you will be prompted for one, this miracle script will promptly remove the root password on first boot.
+
+Your GitHub username is all you need to sudo or su.  No more secrets!
+
+( But wait! Password-less root tty logins are currently available. This is a limited time offer, only valid in LISH ) 
+
+Copyright Marques Johansson, 2014.
+Released under the MIT License
+
+Send comments to @displague
+
---
+++ 10079/stackscript.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# <UDF name="gh_username" Label="GitHub Username" example="GitHub User account to create with sudo access" />
+source <ssinclude StackScriptID=1>
+
+# Fetch GitHub SSH Keys
+function user_github_keys {
+    # Adds the users public key to authorized_keys for the specified user. Make sure you wrap your input variables in double quotes, or the key may not load properly.
+    #
+    #
+    # $1 - Required - username
+    USERNAME="${1}"
+    GITHUBKEYS="https://github.com/${1}.keys"
+    
+    if [ ! -n "$USERNAME" ]; then
+        echo "Must provide a username"
+        return 1;
+    fi
+    
+    if [ "$USERNAME" == "root" ]; then
+        mkdir /root/.ssh
+        wget -q -O- "${GITHUBKEYS}" >> /root/.ssh/authorized_keys
+        return 1;
+    else
+        mkdir -p /home/$USERNAME/.ssh
+        wget -q -O- "${GITHUBKEYS}" >> /home/$USERNAME/.ssh/authorized_keys
+        chown -R "$USERNAME":"$USERNAME" /home/$USERNAME/.ssh
+    fi
+}
+
+echo "#################"
+echo "########"
+echo "########  G I T H U B :"
+echo "  ======                $GH_USERNAME"
+echo "  ======"
+echo ""
+echo "Creating user..."
+user_add_sudo "$GH_USERNAME" "$(randomString)"
+passwd -d displague
+sed -Ei 's/#?\s*(auth\s+sufficient\s+pam_wheel.so\s+trust)/\1/' /etc/pam.d/su
+sed -Ei "s/^root:.*/\0$GH_USERNAME/" /etc/group
+echo "$GH_USERNAME ALL=NOPASSWD: ALL" > "/etc/sudoers.d/$GH_USERNAME"
+echo "Adding GitHub SSH Keys..."
+user_github_keys "$GH_USERNAME"
+echo "Disabling Root SSH..."
+ssh_disable_root
+echo "Disabling Root Password..."
+passwd -d root
+echo -e "\n\nPasswords have been disabled.\nUse SSH ssh://$GH_USERNAME@$(hostname -A)" >> /etc/issue
+echo "Done."
+echo ""
+echo "  ======"
+echo "########"
+echo "########"
